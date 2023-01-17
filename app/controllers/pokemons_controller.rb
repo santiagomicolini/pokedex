aa348000c3ed814a38e5c870dd11d09f2d832325
Santiago Micolini <santi.micolini2@gmail.com>
2023-01-17 20:22:48 -0300

    show page pokemons

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,9 +1,17 @@
 class PokemonsController < ApplicationController
+
+  before_action :set_pokemon, only: [:show]
+
   def index
     @pokemons = Pokemon.all
   end
 
   def show
+  end
 
+  private
+
+  def set_pokemon
+    @pokemon = Pokemon.find(params[:id])
   end
 end
