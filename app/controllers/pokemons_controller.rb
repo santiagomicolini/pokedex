9f37c16f70fe56717774c9f9daf5c7cf18b0579a
Santiago Micolini <santi.micolini2@gmail.com>
2023-01-19 15:56:51 -0300

    organization of the code

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -2,9 +2,13 @@ class PokemonsController < ApplicationController
   before_action :set_pokemon, only: [:show]
 
   def index
+    # I use if else statment to check if there is query present in the search bar.
     if params[:search].present?
+      # In case that we have a query, it will search for th items contain in the
+      # query params in the tables that I define in pg_searh in the model
       @pokemons = Pokemon.search_pokemons(params[:search])
     else
+      # In case that there isn'n query params present it will show all the pokemons.
       @pokemons = Pokemon.all
     end
   end
